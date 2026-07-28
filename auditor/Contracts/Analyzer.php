282fcc2ce4c43ebf402331f6a60d17fd10972273
ashdownbadd <u.randalljay00@gmail.com>
2026-07-28 10:29:32 +0800

    Fixed

--- auditor/Contracts/Analyzer.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-interface Analyzer
-{
-    public function analyze(FileIndex $files): array;
-}
--- cleanup.php
+++ cleanup.php
@@ -7,15 +7,21 @@ require __DIR__ . '/auditor/bootstrap.php';
 $config = require AUDITOR_ROOT . '/config.php';
 
 $console = new Console();
-
 $scanner = new Scanner($config);
 
-$dependencyAnalyzer = new DependencyAnalyzer();
+$dependencyAnalyzer = new DependencyAnalyzer([
+    new IncludeResolver(),
+    new RequireResolver(),
+    new RenderResolver(),
+]);
+
+$unusedFileAnalyzer = new UnusedFileAnalyzer();
 
 $auditor = new Auditor(
     $console,
     $scanner,
-    $dependencyAnalyzer
+    $dependencyAnalyzer,
+    $unusedFileAnalyzer
 );
 
 $auditor->run();
